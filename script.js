db773bb53c51db31f55690719df433244f8813ec
unknown <luizfelipemlds65@gmail.com>
2023-06-13 14:33:25 -0300

    adicionado JS e ScrollSuave

---
+++ script.js
@@ -0,0 +1,20 @@
+const linksInternos = document.querySelectorAll('a[href^="#"');
+
+function scrollSuave() {
+  function scrollar(link) {
+    link.preventDefault();
+    const id = link.currentTarget.getAttribute("href");
+    const section = document.querySelector(id);
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "center",
+    });
+    console.log(section);
+  }
+
+  linksInternos.forEach((links) => {
+    links.addEventListener("click", scrollar);
+  });
+}
+
+scrollSuave();
