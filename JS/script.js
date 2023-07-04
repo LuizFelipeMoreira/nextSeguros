30ef6a51cc2ecd8394360a7abef6c5fc950c7e4e
unknown <luizfelipemlds65@gmail.com>
2023-07-04 02:17:40 -0300

    tooltip em mapa

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,9 @@
 import scrollSuave from "./scrollSuave.js";
 import perguntaFrequente from "./Perguntas.js";
 import modal from "./modal.js";
+import initTooltip from "./tooltip.js";
 
 scrollSuave();
 perguntaFrequente();
 modal();
+initTooltip();
